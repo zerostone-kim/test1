19428c6e7718679262aaa85539715efaece7dcfd
Kim youngseock <zerostone@msn.com>
2020-04-13 14:25:50 +0900

    Update MovieLense.py

--- MovieLense.py
+++ MovieLense.py
@@ -23,11 +23,8 @@ min_rating_userId = userId_mean[userId_mean.rating == min_rating]
 print('1. 최고평점 userId : ' + ','.join(map(str, max_rating_userId.index.tolist())))
 print('2. 최저평점 userId : ' + ','.join(map(str, min_rating_userId.index.tolist())))
 
-# movie, ratings 두개의 dataframe 을 조인하여 1개의 dataframe 로 만듦
-merge_movies_ratings = pd.merge(movies, ratings, how='inner', on='movieId')
-
 # movieId 기준의 group by 해서 rating 컬럼의 평균을 구함
-movies_rating_mean = merge_movies_ratings.groupby('movieId').mean()
+movies_rating_mean = ratings.groupby('movieId').mean()
 max_rating = movies_rating_mean['rating'].max()  
 min_rating = movies_rating_mean['rating'].min()
 
